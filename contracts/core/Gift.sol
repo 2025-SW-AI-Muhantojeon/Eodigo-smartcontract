c4b14e464444ca9312ec19279f688b46553b64cf
kangbaekho <kbh081004@gmail.com>
2025-12-17 14:51:03 +0900

    chore: 잡다한 수정

--- contracts/core/Gift.sol
+++ contracts/core/Gift.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../interface/IOwnerManager.sol";
@@ -13,22 +12,25 @@ contract Gift is ERC20, ERC2771Context {
     IWhiteList public whiteList;
     IStoreList public storeList;
 
-    mapping (address => uint256) public expiryDate;
+    mapping(address => uint256) public expiryDate;
 
     event CashOut(uint256 storeId, uint256 value);
 
-    modifier onlyOwner {
-        require(_msgSender() == ownerManager.owner(), "caller is not the owner");
+    modifier onlyOwner() {
+        require(
+            _msgSender() == ownerManager.owner(),
+            "caller is not the owner"
+        );
         _;
     }
 
-    modifier onlyWhiteList {
+    modifier onlyWhiteList() {
         require(whiteList.whiteList(_msgSender()), "address is not whiteList");
         _;
     }
 
     constructor(
-        address forwarder, 
+        address forwarder,
         address _ownerManager,
         address _storeList,
         address _whiteList
@@ -40,13 +42,14 @@ contract Gift is ERC20, ERC2771Context {
 
     // 만료되었는지 확인, 만약 만료되었을 경우에는 소각
     function cashExpiredCheck(address _address) public onlyOwner {
-        if (block.timestamp > expiryDate[_address]) _burn(_address, balanceOf(_address));
+        if (block.timestamp > expiryDate[_address])
+            _burn(_address, balanceOf(_address));
     }
 
     // 발급
-    function mint(address to, uint256 value) onlyOwner public {
+    function mint(address to, uint256 value) public onlyOwner {
         _mint(to, value);
-        
+
         expiryDate[to] = block.timestamp + 180 days; // 6개월
     }
 
@@ -54,23 +57,29 @@ contract Gift is ERC20, ERC2771Context {
     function transfer(
         address to,
         uint256 value
-    ) public onlyWhiteList override returns(bool) {
+    ) public override onlyWhiteList returns (bool) {
         expiryDate[_msgSender()] = block.timestamp + 180 days; // 6개월
 
         // Store 유효성 검사
         uint256 storeId = storeList.storeIdByAddress(to);
         IStoreList.Store memory store = storeList.storeList(storeId);
-        require(store.wallet != address(0) && store.status, "Incorrect Store Address");
+        require(
+            store.wallet != address(0) && store.status,
+            "Incorrect Store Address"
+        );
 
         return super.transfer(store.wallet, value);
     }
 
     // 정산
-    function settlement(uint256 value) public  {
+    function settlement(uint256 value) public {
         // Store 유효성 검사
         uint256 storeId = storeList.storeIdByAddress(_msgSender());
         IStoreList.Store memory store = storeList.storeList(storeId);
-        require(store.wallet != address(0) && store.status, "Incorrect Store Address");
+        require(
+            store.wallet != address(0) && store.status,
+            "Incorrect Store Address"
+        );
         require(!(balanceOf(_msgSender()) < value), "Balance Too Low");
 
         // 정산량 만큼 소각
@@ -84,15 +93,30 @@ contract Gift is ERC20, ERC2771Context {
         return 0;
     }
 
-    function _msgSender() internal view override(Context, ERC2771Context) returns (address sender) {
+    function _msgSender()
+        internal
+        view
+        override(Context, ERC2771Context)
+        returns (address sender)
+    {
         return ERC2771Context._msgSender();
     }
 
-    function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata) {
+    function _msgData()
+        internal
+        view
+        override(Context, ERC2771Context)
+        returns (bytes calldata)
+    {
         return ERC2771Context._msgData();
     }
 
-    function _contextSuffixLength() internal view override(Context, ERC2771Context) returns (uint256) {
+    function _contextSuffixLength()
+        internal
+        view
+        override(Context, ERC2771Context)
+        returns (uint256)
+    {
         return super._contextSuffixLength();
     }
 }
